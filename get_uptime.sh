1bdbc924a73353ac60948d910be449d69b3ca92c
Jason Haas <jasonrhaas@gmail.com>
2014-05-22 13:16:41 -0400

    added tuptime.pl -u at the end of the script

--- get_uptime.sh
+++ get_uptime.sh
@@ -3,6 +3,11 @@
 # This bash script goes out to each server and writes the data in /proc/upti    me
 # to /usr/share/tuptime/$SERVER/uptime for each server
 
+# This script is meant to run as a cron job so that the servers that are being statused
+# always have up to date information.
+
+# Note that `tuptime -i` needs to be run first to initialize all the directories and files
+
 FILE='servers.conf'
 if [ ! -e $FILE ]; then
         echo "$FILE does not exist.  Please create the file!"
@@ -40,3 +45,5 @@ do
         ssh jhaas@$server cat /proc/uptime > "$UPTIME_DIR$server/uptime" 
         ssh jhaas@$server cat /proc/stat > "$UPTIME_DIR$server/stat"
 done
+
+./tuptime.pl -u  # run the tuptime update command after the data has been grabbed from the servers
